8ba5f8fade1ba34f73b83602d9eca806fae5c293
Quentin Bechade <quentin@MacBook-Pro-de-Quentin.local>
2023-12-06 11:34:46 +0100

    connexion prisma

--- src/bdd.py
+++ src/bdd.py
@@ -1,74 +1,75 @@
-import http.client
-import json
-from urllib.parse import quote
-import sqlite3
-from requests import search_playlist_id, search_track, search_playlist
+from prisma import PrismaClient
 from access_token import get_access_token
+from requests import search_playlist_id, search_playlist
+import sqlite3
 
 # Obtenez le token
 access_token = get_access_token()
 
-#Recherche de l'id de la playlist
-playlist_name=input("Entrez le nom du thème à rechercher: ")
+# Initialisez le client Prisma
+prisma = PrismaClient()
 
-#Effectuer la recherche de la playlist
-result_playlist_id=search_playlist_id(access_token,playlist_name)
+playlist_name = ["Pop mix", "Rock", "Années 80", "Années 90", "Musique de film", "Jazz", "R&B", "Country", "Dubstep",
+                 "Hip-Hop", "Rap", "Rap Francais", "Rap US", "Beurette à chicha", "Funk", "Années 2000", "Afrotrap",
+                 "Bachata", "Latino", "Dua Lipa"]
 
-for playlist_info in result_playlist_id.get("playlists", {}).get("items", []):
-    playlist_id = playlist_info.get("id")
-print(playlist_id)
+for theme_only in playlist_name:
+    # Effectuez la recherche de la playlist
+    result_playlist_id = search_playlist_id(access_token, theme_only)
 
-result_playlist=search_playlist(access_token,str(playlist_id))
+    for playlist_info in result_playlist_id.get("playlists", {}).get("items", []):
+        playlist_id = playlist_info.get("id")
+    print(playlist_id)
 
-# Le nom de la piste que vous souhaitez rechercher
-#track_name = input("Entrez le nom de la piste à rechercher: ")
+    result_playlist = search_playlist(access_token, str(playlist_id))
 
-# Effectuez la recherche de la piste avec le token
-#result = search_track(access_token, track_name)
+    # Connect to the database
+    conn = sqlite3.connect("spotify_tracks.db")
+    cursor = conn.cursor()
 
-print("Résultats de la recherche:")
+    # Create the table if it doesn't exist
+    cursor.execute('''
+        CREATE TABLE IF NOT EXISTS tracks (
+            track_id TEXT PRIMARY KEY,
+            name TEXT,
+            artist TEXT,
+            preview_url TEXT,
+            image_url TEXT,
+            theme TEXT
+        )
+    ''')
 
-# Connect to the database
-conn = sqlite3.connect("spotify_tracks.db")
-cursor = conn.cursor()
+    # Loop through each track in the playlist
+    for track_info in result_playlist.get("tracks", {}).get("items", []):
+        # Get the track information
+        track_id = track_info.get("track", {}).get("id")
+        track_name = track_info.get("track", {}).get("name")
+        track_artist = track_info.get("track", {}).get("artists", [{}])[0].get("name")
+        preview_url = track_info.get("track", {}).get("preview_url")
+        image_url = track_info.get("track", {}).get("album", {}).get("images", [{}])[0].get("url")
 
-# Create the table if it doesn't exist
-cursor.execute('''
-    CREATE TABLE IF NOT EXISTS tracks (
-        id TEXT PRIMARY KEY,
-        name TEXT,
-        artist TEXT,
-        preview_url TEXT,
-        image_url TEXT,
-        theme TEXT
-    )
-''')
+        if preview_url is not None:
+            # Utilisez le client Prisma pour insérer ou mettre à jour la base de données
+            prisma.track.upsert(
+                where={"track_id": track_id},
+                create={"name": track_name, "artist": track_artist, "preview_url": preview_url,
+                        "image_url": image_url, "theme": theme_only},
+                update={"name": track_name, "artist": track_artist, "preview_url": preview_url,
+                        "image_url": image_url, "theme": theme_only},
+            )
 
-# Loop through each track in the playlist
-for track_info in result_playlist.get("tracks", {}).get("items", []):
-    # Get the track information
-    track_id = track_info.get("track", {}).get("id")
-    track_name = track_info.get("track", {}).get("name")
-    track_artist = track_info.get("track", {}).get("artists", [{}])[0].get("name")
-    preview_url = track_info.get("track", {}).get("preview_url")
-    image_url = track_info.get("track", {}).get("album", {}).get("images", [{}])[0].get("url")
+            print(f"Nom de la piste: {track_name}")
+            print(f"Artiste: {track_artist}")
+            print(f"ID Spotify: {track_id}")
+            print(f"URL de prévisualisation: {preview_url}")
+            print(f"URL de l'image: {image_url}")
+            print("\n")
 
-    if preview_url is not None:
-        # Insert or replace the information in the database
-        cursor.execute('''
-            INSERT OR REPLACE INTO tracks (id, name, artist, preview_url, image_url, theme)
-            VALUES (?, ?, ?, ?, ?, ?)
-        ''', (track_id, track_name, track_artist, preview_url, image_url,playlist_name))
+    # Commit the changes to the database
+    conn.commit()
 
-        print(f"Nom de la piste: {track_name}")
-        print(f"Artiste: {track_artist}")
-        print(f"ID Spotify: {track_id}")
-        print(f"URL de prévisualisation: {preview_url}")
-        print(f"URL de l'image: {image_url}")
-        print("\n")
+    # Close the connection to the database
+    conn.close()
 
-# Commit the changes to the database
-conn.commit()
-
-# Close the connection to the database
-conn.close()
+# Fermez la connexion à la base de données Prisma
+prisma.disconnect()
